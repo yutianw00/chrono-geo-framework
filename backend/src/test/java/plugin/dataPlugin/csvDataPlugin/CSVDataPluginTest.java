43fa0298a42f80472a7ad1a8c7050a2cae254098
yihangl <yihangl@andrew.cmu.edu>
2022-04-18 16:02:40 -0400

    add tests for importDataFrom

--- backend/src/test/java/plugin/dataPlugin/csvDataPlugin/CSVDataPluginTest.java
+++ backend/src/test/java/plugin/dataPlugin/csvDataPlugin/CSVDataPluginTest.java
@@ -1,21 +1,29 @@
 package plugin.dataPlugin.csvDataPlugin;
 
+import framework.core.DataPlugin;
+import framework.core.utils.MyData;
+import org.junit.Before;
 import org.junit.Test;
+import plugin.dataPlugin.apiDataPlugin.ApiDataPlugin;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class CSVDataPluginTest {
+    private String path;
+    private DataPlugin plugin;
+
+    @Before
+    public void setUp() throws Exception {
+        path = "src/main/resources/dataset/bird_migration.csv";
+        plugin = new CSVDataPlugin();
+    }
 
     @Test
     public void importDataFromFile() {
-    }
-
-    @Test
-    public void dataEqual() {
-    }
-
-    @Test
-    public void group() {
+        List<MyData> records = plugin.importDataFromFile(path);
+        assertTrue(records.size() > 0);
     }
 
     @Test
--- backend/src/test/java/plugin/dataPlugin/excelDataPlugin/ExcelDataPluginTest.java
+++ backend/src/test/java/plugin/dataPlugin/excelDataPlugin/ExcelDataPluginTest.java
@@ -1,22 +1,31 @@
 package plugin.dataPlugin.excelDataPlugin;
 
+import framework.core.DataPlugin;
+import framework.core.utils.MyData;
+import org.junit.Before;
 import org.junit.Test;
+import plugin.dataPlugin.csvDataPlugin.CSVDataPlugin;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class ExcelDataPluginTest {
+    private String path;
+    private DataPlugin plugin;
 
+    @Before
+    public void setUp() throws Exception {
+        path = "src/main/resources/dataset/bird_migration.xlsx";
+        plugin = new ExcelDataPlugin();
+    }
     @Test
     public void importDataFromFile() {
+        List<MyData> records = plugin.importDataFromFile(path);
+        assertTrue(records.size() > 0);
     }
 
-    @Test
-    public void dataEqual() {
-    }
 
-    @Test
-    public void group() {
-    }
 
     @Test
     public void predictFuture() {
